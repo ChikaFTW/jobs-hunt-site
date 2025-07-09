3a66499bb431b8a8ad359a5414c328744b5d9eed
ChikaFTW <salim.7777@live.fr>
2025-07-09 12:38:21 +0100

    updating the default search country for job seekers

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -225,7 +225,7 @@ const config = {
     appId: '4f766f0f', // Keep your actual App ID
     appKey: '423697dcc76a00622c6c9c7323745e42', // Keep your actual App Key
     baseUrl: 'https://api.adzuna.com/v1/api/jobs',
-    defaultCountry: 'fr', // Changed to Singapore as default
+    defaultCountry: 'za', // Changed to Singapore as default
     resultsPerPage: 4,
     defaultKeywords: '',
     categoryMappings: {
